24d7684aafa8890fcd9641aeb7f41bc7a7eb8d4c
PC\okaji <hajimetep010@dynarise1001.xyz>
2025-06-27 17:36:51 +0900

    敵がプレイヤーを見つけるところまで

--- Assets/Scripts/wather.cs
+++ Assets/Scripts/wather.cs
@@ -5,21 +5,55 @@ using UnityEngine;
 
 public class wather : MonoBehaviour
 {
-    public float waitUP = 11f;
-    public Vector3 startposition;  //目線の位置に使うもの
-    public Vector3 direction;  //目線を飛ばす方向
-    public float distansce = 60f;
-    // Start is called before the first frame update
+    public float eyeHeight = 1.5f;
+    public float viewDistance = 60f;
+    public Vector3 direction = Vector3.right;
+    public LayerMask detectionLayer;
+    public bool playerDetected = false;
+
+    private Vector3 eyePosition;
     void Start()
     {
-        //目線の位置
-        startposition = new Vector3(transform.position.x,transform.position.y + 11,transform.position.z);
-        direction = new Vector3(1, 0, 0);  //目線を飛ばす方向はx方向
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //目線の位置を更新（少し上
+        //eyePosition = new Vector3(transform.position.x, transform.position.y + eyeHeight, transform.position.z);
+        //rayを飛ばす
+        //Ray ray = new Ray(eyePosition, transform.TransformDirection(direction));
+        RaycastHit hit;
+        Vector3 origin = transform.position + new Vector3(0, eyeHeight, 0);
+        Vector3 direction = transform.forward;
+
+        //デバッグ表示（Sceneビューで見える）
+        //Debug.DrawRay(eyePosition, transform.TransformDirection(direction) * viewDistance, Color.red);
+
+        //視線に何かぶつかったか判定
+        if (Physics.Raycast(origin, direction,out hit, viewDistance, detectionLayer))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                if (!playerDetected)
+                {
+                    playerDetected = true;
+                    Debug.Log("プレイヤーを発見");
+                    //ここでゲームオーバーやアラームの処理を呼ぶ
+                }
+            }
+        }
+        else
+        {
+            playerDetected = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 origin = transform.position + new Vector3(0, eyeHeight, 0);
+        Vector3 direction = transform.forward * viewDistance;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin+direction);
     }
 }
