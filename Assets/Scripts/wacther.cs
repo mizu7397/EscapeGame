37d18e71a91992c05d7eed8569fe9f0277577fe8
PC\okaji <hajimetep010@dynarise1001.xyz>
2025-07-04 18:34:29 +0900

    障害物ウォッチの途中

---
+++ Assets/Scripts/wacther.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class wacther : MonoBehaviour
+{
+    public float eyeHeight = 1.5f;              //目の高さ
+    public float viewDistance = 60f;            //視線の届く距離
+    public Vector3 direction = Vector3.right;   //視線の方向
+    public LayerMask playerLayer;            //プレイヤーが属するレイヤー
+    public bool playerDetected = false;         //プレイヤーを見つけたかどうか
+    public float checkIntervel = 0.5f;          //チェックの時間（秒）
+    public LayerMask blockLayer; //障害物のレイヤー 
+
+    private Vector3 eyePosition;
+    void Start()
+    {
+        //一定間隔でCheckPlayerを繰り返す（0秒後に開始し、checkInteval1秒ごと）
+        InvokeRepeating("CheckPlayer",0f,checkIntervel);
+    }
+
+    // Update is called once per frame
+    void CheckPlayer()
+    {
+        playerDetected = false;
+        Debug.Log("チェック開始");
+       //ジャイアントの位置＋アイハイト分の高さ
+        Vector3 origin = transform.position + new Vector3(0, eyeHeight, 0);
+        //ジャイアントの前方
+        Vector3 direction = transform.forward;
+
+        RaycastHit[] hits;　//配列　※複数の値を格納するもの
+
+        hits = Physics.RaycastAll(
+            origin,//目線の場所
+            direction,//方向
+            viewDistance//距離
+            );
+
+        //繰り返し構文　for分
+        //hitsの中に格納したオブジェクトの点検を繰り返す
+        //far (どのくらい繰り返すかの条件を書く){繰り返したい処理}
+        
+        // int iはカウント変数
+        //①カウンター変数iの宣言と最初の数を決める
+        //②iがいくつか繰り返すのかを決めている。//hits配列の中の数（長さ）Length
+        //③何個飛ばしで数えるか。（数える回数に違いが出る）変数名++というのはその変数を1ずつ増やす
+        for ( int i = 0; i < hits.Length; i++)
+        {
+            GameObject obj = hits[i].collider.gameObject;
+            //障害物レイヤーかどうかチェック
+            if (IsInLayerMask(obj,blockLayer))
+            {
+                Debug.Log("障害物によりセーフ");
+            }
+        }
+
+
+        //視線に何かぶつかったか判定
+        //if (Physics.Raycast(origin, direction,out hit, viewDistance, playerLayer))
+        //{
+        //    if (hit.collider.CompareTag("Player"))
+        //    {
+        //        if (!playerDetected)
+        //        {
+        //            playerDetected = true;
+        //            Debug.Log("プレイヤーを発見");
+        //            //ここでゲームオーバーやアラームの処理を呼ぶ
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    playerDetected = false;
+        //}
+    }
+
+    //レイヤーをチェックするメソッド
+    bool IsInLayerMask(GameObject obj, LayerMask mask)
+    {
+        return ((1 << obj.layer) & mask) != 0;
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 origin = transform.position + new Vector3(0, eyeHeight, 0);
+        Vector3 direction = transform.forward * viewDistance;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin+direction);
+    }
+}
